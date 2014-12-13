64c01d0f3269e7ed2da562e6b022f92d6569261a
megazear7 <alexlockhart7@gmail.com>
2014-12-12 23:23:34 -0500

    layout started

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :require_login
+
+  private
+
+    def require_login
+      if not current_user and not params[:controller].include? "devise"
+        redirect_to new_user_session_path
+      end
+    end
+
 end
