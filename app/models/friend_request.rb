26a026e6a3387de546f4860a8e14b46f4079838f
megazear7 <alexlockhart7@gmail.com>
2014-12-14 16:11:05 -0500

    onlu applicable users should appear on the send friend request link now

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,4 +1,8 @@
 class FriendRequest < ActiveRecord::Base
   belongs_to :requester, class_name: "User"
   belongs_to :requestee, class_name: "User"
+
+  def self.relevant_users_for user
+    (User.all.to_a - user.friends.to_a) - [user]
+  end
 end
