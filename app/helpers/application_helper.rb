741118c5212eb530b3000ded68264fcbe6ec1d53
megazear7 <alexlockhart7@gmail.com>
2014-12-15 22:50:44 -0500

    styled links buttons and input elements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def active_link_to text, path, html_options
+    tmp = Rails.application.routes.recognize_path(path, method: html_options[:method])
+    cssClass = params[:action] == tmp[:action] && params[:controller] == tmp[:controller] ? " active" : ""
+    html_options[:class] = html_options[:class] + cssClass
+    link_to text, path, html_options 
+  end
 end
